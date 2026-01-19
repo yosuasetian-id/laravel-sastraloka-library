168b19f82dcb34a564e5126c77da156629804007
yosuasetian-id <joshuasetianugraha@gmail.com>
2026-01-19 17:40:11 +0700

    make logout

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,12 @@ use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
