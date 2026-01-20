09f2a4bc3d207b7d9a2523cd659459a8d4b62653
yosuasetian-id <joshuasetianugraha@gmail.com>
2026-01-20 22:57:46 +0700

    profile controller index (panding)

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -7,12 +7,16 @@ use App\Http\Requests\UpdateProfileRequest;
 
 class ProfileController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:sanctum');
+    }
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        return Profile::with('user')->get();
     }
 
     /**
