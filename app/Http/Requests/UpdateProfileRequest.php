547098ae022d31e5d9490dc8d2216fba09b25bd1
yosuasetian-id <joshuasetianugraha@gmail.com>
2026-01-20 22:49:48 +0700

    make profile request

--- app/Http/Requests/UpdateProfileRequest.php
+++ app/Http/Requests/UpdateProfileRequest.php
@@ -11,7 +11,7 @@ class UpdateProfileRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,24 @@ class UpdateProfileRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'first_name' => 'nullable|string',
+            'last_name' => 'nullable|string',
+            'place_of_birth' => 'nullable|string',
+            'date_of_birth' => 'nullable|date',
+            'address' => 'nullable|string',
+            'phone_number' => 'nullable|numeric',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'first_name.string' => 'Nama depan harus berupa teks.',
+            'last_name.string' => 'Nama belakang harus berupa teks.',
+            'place_of_birth.string' => 'Tempat lahir harus berupa teks.',
+            'date_of_birth.date' => 'Tanggal lahir harus berupa tanggal.',
+            'address.string' => 'Alamat harus berupa teks.',
+            'phone_number.numeric' => 'Nomer telepon harus berupa angka.'
         ];
     }
 }
