25380decc1b82dea7ace4d55d5418593da668cc6
yosuasetian-id <joshuasetianugraha@gmail.com>
2026-01-21 23:02:09 +0700

    edit Profile policy

--- app/Policies/ProfilePolicy.php
+++ app/Policies/ProfilePolicy.php
@@ -6,4 +6,10 @@ use App\Models\Profile;
 use App\Models\User;
 use Illuminate\Auth\Access\Response;
 
-class ProfilePolicy {}
+class ProfilePolicy
+{
+    public function update(User $user, Profile $profile): Response
+    {
+        return $user->id === $profile->user_id ? Response::allow() : Response::deny('Anda bukan pemilik data.');
+    }
+}
