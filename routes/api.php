1b023d4720a564358023bedc4b7439085c1fb263
yosuasetian-id <joshuasetianugraha@gmail.com>
2026-01-22 21:14:47 +0700

    edit api delete account

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,8 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/email/verify/{id}/{hash}', [AuthController::class, 'verify'])->name('verification.verify');
+Route::get('/user/delete/confirm/{id}/{hash}', [AuthController::class, 'confirmDeleteAccount'])
+    ->name('user.delete.confirm');
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/email/resend', [AuthController::class, 'resend']);
@@ -16,8 +18,6 @@ Route::middleware('auth:sanctum')->group(function () {
     });
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/user/delete/request', [AuthController::class, 'deleteAccount']);
-    Route::get('/user/delete/confirm/{id}/{hash}', [AuthController::class, 'confirmDeleteAccount'])
-        ->name('user.delete.confirm');
 });
 
 Route::apiResource('profile', ProfileController::class);
